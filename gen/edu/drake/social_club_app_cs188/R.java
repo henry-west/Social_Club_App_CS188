c3b7543099e5299878c602369f0565fcc3fcf273
nsGiuliani <nicholas.giuliani@drake.edu>
2014-09-11 14:49:38 -0500

    added the screens

--- gen/edu/drake/social_club_app_cs188/R.java
+++ gen/edu/drake/social_club_app_cs188/R.java
@@ -29,14 +29,37 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_screen2=0x7f030001;
+        public static final int activity_screen2a=0x7f030002;
+        public static final int activity_screen3=0x7f030003;
+        public static final int activity_screen3a=0x7f030004;
+        public static final int activity_screen4=0x7f030005;
+        public static final int activity_screen5=0x7f030006;
+        public static final int activity_screen6=0x7f030007;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int screen1a=0x7f070001;
+        public static final int screen2=0x7f070002;
+        public static final int screen2a=0x7f070003;
+        public static final int screen3=0x7f070004;
+        public static final int screen3a=0x7f070005;
+        public static final int screen4=0x7f070006;
+        public static final int screen5=0x7f070007;
+        public static final int screen6=0x7f070008;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int title_activity_screen1a=0x7f050003;
+        public static final int title_activity_screen2=0x7f050004;
+        public static final int title_activity_screen2a=0x7f050005;
+        public static final int title_activity_screen3=0x7f050006;
+        public static final int title_activity_screen3a=0x7f050007;
+        public static final int title_activity_screen4=0x7f050008;
+        public static final int title_activity_screen5=0x7f050009;
+        public static final int title_activity_screen6=0x7f05000a;
     }
     public static final class style {
         /** 
