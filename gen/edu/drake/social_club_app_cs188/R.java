d12db2a02b7586df3e747864458102e28727fd8b
Meredith Moore <Meredith.Moore011@gmail.com>
2014-09-16 22:10:44 -0500

    Added the pictures to the screens. They don't take up the whole screen, and I will probably ask Urness about this tomorrow. Next step is to add the paths togethert and make the buttons functional

--- gen/edu/drake/social_club_app_cs188/R.java
+++ gen/edu/drake/social_club_app_cs188/R.java
@@ -36,7 +36,8 @@ public final class R {
         public static final int suggested=0x7f02000b;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int imageButton1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_calendar=0x7f030000;
